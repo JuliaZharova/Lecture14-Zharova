ca84b4b444268f0cf98b3cc68b1dc3bc31285aaa
Julia Zharova <julia.amp.zharova@gmail.com>
2022-01-24 13:42:03 +0200

    Fixed

--- src/main/java/ClassForMethods.java
+++ src/main/java/ClassForMethods.java
@@ -12,16 +12,17 @@ public class ClassForMethods {
         driver.manage().window().maximize();
         try {
             driver.get("https://rozetka.com.ua");
-            WebElement searchField = driver.findElement(By.xpath("//input[@class]"));
+            WebElement searchField = driver.findElement(By.xpath("//input[@type=\"text\"]"));
             searchField.click();
-            searchField.sendKeys(new CharSequence[]{"Автономная солнечная станция Risen 5 кВт/час"});
+            searchField.sendKeys("Автономная солнечная станция Risen 5 кВт/час");
             System.out.println("Search field is displayed " + searchField.isDisplayed());
             searchField.sendKeys(Keys.ENTER);
 
-            driver.get("https://rozetka.com.ua/272097571/p272097571/");
             System.out.println("Current URL is " + driver.getCurrentUrl());
+            new WebDriverWait(driver, Duration.ofSeconds(10))
+                    .until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@class=\"buy-button button button_with_icon button_color_green button_size_large ng-star-inserted\"]")));
 
-            WebElement buyButton = driver.findElement(By.xpath("//*[@id=\"#scrollArea\"]/div[1]/div[2]/rz-product-main-info/div[2]/div/ul/li[1]/app-product-buy-btn/app-buy-button/button"));
+            WebElement buyButton = driver.findElement(By.xpath("//button[@class=\"buy-button button button_with_icon button_color_green button_size_large ng-star-inserted\"]"));
             System.out.println("Search button is displayed " + buyButton.isDisplayed());
             buyButton.click();
 
@@ -52,7 +53,9 @@ public class ClassForMethods {
             System.out.println("Search field is displayed " + catalogButton.isDisplayed());
             catalogButton.click();
 
-            WebElement category = driver.findElement(By.xpath("//ul/li[1]/div/div[2]/div[1]/div[2]/ul[2]/li/ul/li[1]/a"));
+            new WebDriverWait(driver, Duration.ofSeconds(30))
+                    .until(ExpectedConditions.elementToBeClickable(By.linkText("Мониторы")));
+            WebElement category = driver.findElement(By.linkText("Мониторы"));
             System.out.println("Search category is displayed " + category.isDisplayed());
             category.click();
             try {
@@ -69,9 +72,9 @@ public class ClassForMethods {
             String textSearchResult = searchResult.getText();
             searchResult.click();
             new WebDriverWait(driver, Duration.ofSeconds(30))
-                    .until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//h1")));
-            WebElement selectedItem = driver.findElement(By.xpath("//h1"));
-            System.out.println("Selected item is enabled " + selectedItem.isEnabled());
+                    .until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//h1[@class=\"product__title\"]")));
+            WebElement selectedItem = driver.findElement(By.xpath("//h1[@class=\"product__title\"]"));
+            System.out.println("Selected item is displayed " + selectedItem.isDisplayed());
             String textSelectedItem = selectedItem.getText();
 
             WebElement rozetkaSeller = driver.findElement(By.xpath("//*[@id=\"#scrollArea\"]//rz-marketplace-link/strong"));
